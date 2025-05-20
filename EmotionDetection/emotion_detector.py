8e100aaa6a101f615ecd06b46a0362f1e62ed266
Chigoziem Edmund Ofili <goziem.ofili@gmail.com>
2025-05-20 16:28:41 -0600

    Update emotion_detector.py

--- EmotionDetection/emotion_detector.py
+++ EmotionDetection/emotion_detector.py
@@ -23,5 +23,14 @@ def emotion_detector(text_to_analyse):
             'sadness': emotion_predictions['sadness'],
             'dominant_emotion': dominant_emotion
         }
+    elif response.status_code == 400:
+        return {
+            'anger': None,
+            'disgust': None,
+            'fear': None,
+            'joy': None,
+            'sadness': None,
+            'dominant_emotion': None
+        }
     else:
         return None
