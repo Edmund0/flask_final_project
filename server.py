444d8fbc050f8a6a268560f65ccfa16ad385a9ad
Chigoziem Edmund Ofili <goziem.ofili@gmail.com>
2025-05-20 17:02:49 -0600

    Update server.py

--- server.py
+++ server.py
@@ -1,23 +1,29 @@
+"""This module does such and such."""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
-app = Flask("Emotional Damage");
+
+app = Flask("Emotional Damage")
 
 @app.route('/')
-def Home():
+def home():
+    '''Demonstrates triple double quotes
+    docstrings and does nothing really.'''
     return render_template("index.html")
 
 @app.route('/emotionDetector')
-def emotionDetector():
+def emotion_detection():
+    '''Demonstrates triple double quotes
+    docstrings and does nothing really.'''
     text_to_analyze = request.args.get('textToAnalyze')
     main_response = emotion_detector(text_to_analyze)
 
-    if (main_response['dominant_emotion'] is None):
+    if main_response['dominant_emotion'] is None:
         result_text = "Invalid text! Please try again!"
     else:
         dominant_emotion = main_response['dominant_emotion']
-        result_text = f"For the given statement, the system response is {main_response}. The dominant emotion is {dominant_emotion}"
-
-    return result_text 
+        result_text_1 = f"For the given statement, the system response is {main_response}."
+        result_text = result_text_1 + f" The dominant emotion is {dominant_emotion}"
+    return result_text
 
 if __name__ == "__main__":
     app.run(host="localhost", port=5000)
